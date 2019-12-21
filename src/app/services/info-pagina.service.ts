d282fbdbc80d010ad2d9df0c31fdb6cfd99fb53b
Jim Zapata <jimzr6@gmail.com>
2019-12-21 15:29:55 -0500

    Fin sección 7

---
+++ src/app/services/info-pagina.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { InfoPagina } from '../interfaces/info-pagina.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+
+  info: InfoPagina = {};
+  cargada = false;
+
+  equipo: any[] = [];
+
+  constructor(private http: HttpClient) {
+    // console.log('servicio de infoPagina listo');
+
+    this.cargarInfo();
+    this.cargarEquipo();
+   
+   }
+    
+   private cargarInfo(){
+     //leer el archivo json
+    this.http.get('assets/data/data-pagina.json')
+    .subscribe( (resp: InfoPagina) =>{
+      this.cargada = true;
+      this.info = resp;
+      // console.log(resp);  
+    });
+   }
+
+   private cargarEquipo() {
+    this.http.get('https://angular-portafolio-cae17.firebaseio.com/equipo.json')
+    .subscribe( (resp: any[]) =>{
+      this.equipo = resp;
+      // console.log(resp);  
+    });
+   }
+     
+}
